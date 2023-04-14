9a18ba8650d1254e9b96449cdfa1e3650c4c4147
gracezhong <grace.zhong@powerhealthsolutions.com>
2023-04-14 15:32:33 +0930

    Initial commit

---
+++ spring-soap-ws/README.md
@@ -0,0 +1,3 @@
+Spring + jaxb2-maven-plugin + Gradle + Java17 Example
+
+Following https://spring.io/guides/gs/producing-web-service/
---
+++ spring-soap-ws/src/test/java/grace/springsoapws/SpringSoapWsApplicationTests.java
@@ -0,0 +1,13 @@
+package grace.springsoapws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoapWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
